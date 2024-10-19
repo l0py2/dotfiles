99d5ed1e02a99000952c0617d667d13c6e4d2548
l0py2 <l0py2.git@gmail.com>
2024-10-19 22:11:15 +0100

    Moved home related files to a different directory

--- presets/base.sh
+++ presets/base.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-copy common "$HOME"
+copy common/home "$HOME"
+
+mkdir -p "$HOME/.config"
+copy common/bash "$HOME/.config"
+copy common/git "$HOME/.config"
+copy common/shell "$HOME/.config"
