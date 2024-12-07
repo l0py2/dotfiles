47b0891909f1c1e46d672e649e7b2bff67d312c8
l0py2 <l0py2.git@gmail.com>
2024-12-07 11:17:55 +0000

    Neovim configuration added

---
+++ common/nvim/lua/options.lua
@@ -0,0 +1,14 @@
+local o = vim.o
+
+o.expandtab = true
+o.softtabstop = 0
+o.shiftwidth = 6
+o.tabstop = 6
+
+o.autoindent = true
+
+o.number = true
+o.relativenumber = true
+
+o.cursorline = true
+o.cursorcolumn = true
--- presets/hyprland.sh
+++ presets/hyprland.sh
@@ -3,3 +3,4 @@
 copy common/home "$HOME"
 
 copy wayland/hypr "$XDG_CONFIG_HOME/hypr"
+copy common/nvim "$XDG_CONFIG_HOME/nvim"
