48125e819e97001b004cb745c9942f51df7db13a
l0py2 <l0py2.git@gmail.com>
2024-12-24 11:08:38 +0000

    Added Treesitter

---
+++ common/nvim/lua/plugins.lua
@@ -0,0 +1,22 @@
+local lazy_path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+
+if not (vim.uv or vim.loop).fs_stat(lazy_path) then
+	local lazy_repo = 'https://github.com/folke/lazy.nvim.git'
+	local out = vim.fn.system({
+		'git', 'clone', '--filter=blob:none', '--branch=stable',
+		lazy_repo, lazy_path
+	})
+
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+			{ out, 'WarningMsg' }
+		}, true, {})
+	end
+end
+
+vim.opt.rtp:prepend(lazy_path)
+
+require('lazy').setup({
+	{ import = 'plugins/treesitter' }
+})
