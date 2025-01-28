056d90fbac5b211889bbcea462f311ec61f0922c
l0py2 <l0py2.git@gmail.com>
2025-01-28 23:53:26 +0000

    Added configuration for foot

--- presets/hyprland.sh
+++ presets/hyprland.sh
@@ -4,6 +4,7 @@ copy common/home "$HOME"
 
 copy wayland/hypr "$XDG_CONFIG_HOME/hypr"
 copy wayland/waybar "$XDG_CONFIG_HOME/waybar"
+copy wayland/foot "$XDG_CONFIG_HOME/foot"
 copy common/nvim "$XDG_CONFIG_HOME/nvim"
 copy common/mpd "$XDG_CONFIG_HOME/mpd"
 copy common/ncmpcpp "$XDG_CONFIG_HOME/ncmpcpp"
