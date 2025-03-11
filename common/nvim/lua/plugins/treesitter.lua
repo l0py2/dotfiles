512e60894d5f7a13f4d2342ad3eaf05c177611c5
l0py2 <l0py2.git@gmail.com>
2025-03-11 22:44:48 +0000

    Added nvim-treesitter configuration

--- common/nvim/lua/plugins/treesitter.lua
+++ common/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,12 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
 	build = ':TSUpdate',
-	opts = {}
+	config = function()
+		require('nvim-treesitter.configs').setup {
+			auto_install = true,
+			highlight = {
+				enable = true
+			}
+		}
+	end
 }
