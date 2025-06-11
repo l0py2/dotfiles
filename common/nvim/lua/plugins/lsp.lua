4d9c1472d49178a4b67ce835a303d4cc6d524736
l0py2 <l0py2.git@gmail.com>
2025-06-11 17:34:18 +0100

    Activated nvim-cmp capabilities for all LSP clients

--- common/nvim/lua/plugins/lsp.lua
+++ common/nvim/lua/plugins/lsp.lua
@@ -24,12 +24,8 @@ return {
 	config = function()
 		local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-		--[[ require('mason-lspconfig').setup_handlers {
-			function (server_name)
-				require('lspconfig')[server_name].setup {
-					capabilities = cmp_capabilities
-				}
-			end
-		} ]]--
+		vim.lsp.config('*', {
+			capabilities = cmp_capabilities
+		})
 	end
 }
